5502c4597e15c5897373bb6a6983dfb59c82d9d1
johnkirtley <kirtleyj16@gmail.com>
2020-03-12 10:40:05 -0700

    Added env file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // read from .env file located within root folder
+
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 server.listen(port, () => {
 	console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
